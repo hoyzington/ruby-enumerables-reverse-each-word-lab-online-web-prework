6ca2a65c6df76d9f1cf9b79c956cad95cba3a552
Scott Hoisington <hoyzington@gmail.com>
2019-11-12 09:32:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
-  array = string.scan(/\w+/)
+  array = string.scan(/\bw+\b/)
   reverse = array.collect{|word| word.reverse}
   new_string = reverse.join(" ")
   new_string
 end
 
+(/\b[AEIOUaeiou][a-z]*\b/)
