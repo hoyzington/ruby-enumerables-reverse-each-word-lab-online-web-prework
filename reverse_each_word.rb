c15ee33beb46d193e35cee06a19e47150771e309
Scott Hoisington <hoyzington@gmail.com>
2019-11-12 18:26:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,6 @@
-#def reverse_each_word(string)
-#  array = string.scan(/\S+/)
-#  reverse = array.collect{|word| word.reverse}
-#  new_string = reverse.join(" ")
-#  new_string
-#end
-
 def reverse_each_word(string)
-  string.scan(/\S+/)
-  string.collect{|word| word.reverse}
-  string.join(" ")
-  string
+  array = string.scan(/\S+/)
+  reverse = array.collect{|word| word.reverse}
+  new_string = reverse.join(" ")
+  new_string
 end
