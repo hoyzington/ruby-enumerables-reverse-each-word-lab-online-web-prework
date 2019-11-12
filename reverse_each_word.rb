b3ea204fac8c538b78def64ebacb636058589321
Scott Hoisington <hoyzington@gmail.com>
2019-11-12 18:04:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
-  array = string.scan(/\bw*\b/)
+  array = string.scan(/\bw+\b/)
 #  reverse = array.collect{|word| word.reverse}
 #  new_string = reverse.join(" ")
 #  new_string
